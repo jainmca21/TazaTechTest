bc1218e88e69d3527d39349f012ea319f658d623
Rahul Jain <rahul.jain@harman.com>
2021-01-19 22:10:10 +0530

    Added button to play sound track in Detail screen

--- TaazaTest/screens/Detail.js
+++ TaazaTest/screens/Detail.js
@@ -1,9 +1,24 @@
 import React from 'react'
-import { View ,Text, TouchableOpacity, StyleSheet} from 'react-native'
-
+import { View ,Text, TouchableOpacity, StyleSheet, Button} from 'react-native'
+import SoundPlayer from 'react-native-sound-player'
+import NetInfo from "@react-native-community/netinfo";
+import { useEffect } from 'react'
 
 export const Detail = (props) =>{
     console.log(JSON.stringify(props.route.params))
+
+
+
+    function playTrack(url) {
+        NetInfo.fetch().then(state => {
+            if(state.isConnected){
+                SoundPlayer.playUrl(url)
+            }else{
+                alert("Please check your internet.")
+            }
+        })
+    }
+
     return(
         <View style = {{alignSelf:'center'}}>
         
@@ -12,6 +27,10 @@ export const Detail = (props) =>{
         <Text >{`Track Price ${props.route.params.trackPrice}`}</Text>
         <Text >{`Release Date ${props.route.params.releaseDate}`}</Text>
         <Text >{`Collection Price ${props.route.params.collectionPrice}`}</Text>
+
+        <Button title='Play Track' onPress={()=>{
+            playTrack(props.route.params.previewUrl)
+        }}/>
         </View>
     )
 }
