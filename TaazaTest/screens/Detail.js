64a0f2da2649abccc9c1921b4a0e8dfbcd960ac3
Rahul Jain <rahul.jain@harman.com>
2021-01-19 20:37:43 +0530

    Setup navigation stack

---
+++ TaazaTest/screens/Detail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View ,Text, TouchableOpacity, StyleSheet} from 'react-native'
+
+
+export const Detail = () =>{
+    return(
+        <View>
+        
+        <Text > Detial screen</Text>
+        </View>
+       
+    )
+}
---
+++ TaazaTest/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View ,Text, TouchableOpacity, StyleSheet} from 'react-native'
+
+
+export const Home = () =>{
+    return(
+        <View >
+            <Text> Home screen</Text>
+        </View>
+       
+    )
+}
+
+
